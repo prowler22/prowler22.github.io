af47cb44ff664ab683448a864a448342137eadf4
prowler22 <prowler22@gmail.com>
2021-07-16 13:42:51 -0400

    Update to Weekly list
    Update to Weekly list

--- linksw.php
+++ linksw.php
@@ -1,41 +1,38 @@
 <?php
-define('URL', 'https://marvel.fandom.com/wiki/Category:%YEAR%,_%MONTH%');
-define('DCURL', 'https://dc.fandom.com/wiki/Category:%YEAR%,_%MONTH%');
+define('URL', 'http://www.mikesamazingworld.com/mikes/features/newsstand.php?type=calendar&month=%MONTH%&year=%YEAR%&publisher=marvel&sort=date&checklist=on&variantex=on&collectionex=on');
+define('DCURL', 'http://www.mikesamazingworld.com/mikes/features/newsstand.php?type=calendar&month=%MONTH%&year=%YEAR%&publisher=dc&sort=date&checklist=on&variantex=on&collectionex=on');
 define('TOP', "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE xml>\n");
 $startYear = 1939;
 $startURL = URL;
 $dc = "";
 $project = "CMC";
 if (isset($argv[1]) && strtolower($argv[1]) == "dc") {
-   $dc = "dc";
+   $dc = "DC";
    $startYear = 1935;
    $startURL = DCURL;
    $project = "CDCC";
 }
 //$years = array('1935');
-$years = range($startYear,1940);
+$years = range($startYear,2020);
 
-$months = array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
+//$months = array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
 
 foreach ($years as $year) {
    echo "Building year: " . $year . "\n";
    $yearContents = "";
 
-   $monthNum = 1;
-   foreach ($months as $month) {
+   //foreach ($months as $month) {
+   for ($month = 1; $month <= 12; $month++) {
       $url = str_replace(array("%YEAR%", "%MONTH%"), array($year, $month), $startURL);
       $website = @file_get_contents($url);
-      if ($website == '' || $website === false) {
-         $monthNum++;
-         continue;
-      }
+      if ($website == '' || $website === false) continue;
 
       $doc = new DOMDocument();
       libxml_use_internal_errors(true);
       $doc->loadHTML($website); // or you could load from a string using loadHTML();
       libxml_clear_errors();
       $xpath = new DOMXpath($doc);
-      $elements = $xpath->query("//div[@class='wikia-gallery-item']//a");
+      $elements = $xpath->query("//table[@class='covertable']//a");
 
       $sep = "\n";
       $monthContents = "";
@@ -60,16 +57,14 @@ foreach ($years as $year) {
       }
 
       if ($monthContents != "") {
-         $yearContents .= '        <Directory Name="' . $project . ' ' . $year . ' ' . str_pad($monthNum, 2, '0', STR_PAD_LEFT) . '">' 
+         $yearContents .= '        <Directory Name="' . $project . ' ' . $year . ' ' . str_pad($month, 2, '0', STR_PAD_LEFT) . '">' 
                           . $monthContents . "\n        </Directory>\n";
       }
-
-      $monthNum++;
    }
 
    echo "Saving year: " . $year . "\n";
    if ($yearContents != "") {
-      file_put_contents($dc . $year . ".xml", TOP . '    <Year Name="' . $project . ' ' . $year . "\">\n" . $yearContents . '    </Year>');
+      file_put_contents("Weekly" . $dc . $year . ".xml", TOP . '    <Year Name="' . $project . ' ' . $year . "\">\n" . $yearContents . '    </Year>');
    }
 }
 ?>
